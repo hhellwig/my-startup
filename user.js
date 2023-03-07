94faa52bdbd677fe63e9ff8bf5dc7fa7e1368bc4
hhellwig <haileymhellwig@gmail.com>
2023-03-06 22:19:52 -0700

    Update(js) user page has username

---
+++ user.js
@@ -0,0 +1,17 @@
+class User {
+    name;
+    password;
+    score;
+    constructor() {
+        const playerNameEl = document.querySelector('.player');
+        playerNameEl.textContent = this.getPlayerName();
+        const scoreEl = document.querySelector('.score');
+        scoreEl.textContent = '0';
+    }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Unknown';
+    }
+}
+
+const user = new User();
