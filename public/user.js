7db54768b4f8090356b5a18910d96915c96f0454
hhellwig <haileymhellwig@gmail.com>
2023-03-24 18:05:12 -0600

    Update(login) login and authentiaction added

--- public/user.js
+++ public/user.js
@@ -20,3 +20,30 @@ function changePassword() {
     const newPass = document.querySelector("#password");
     localStorage.setItem("password", newPass.value);
 }
+
+function displayQuote(data) {
+    const containerEl = document.querySelector("#quote");
+  
+    const quoteEl = document.createElement("p");
+    quoteEl.classList.add("quote");
+    const authorEl = document.createElement("p");
+    authorEl.classList.add("author");
+  
+    quoteEl.textContent = data.content;
+    authorEl.textContent = data.author;
+  
+    containerEl.appendChild(quoteEl);
+    containerEl.appendChild(authorEl);
+  }
+  
+  function callService(url, displayCallback) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        displayCallback(data);
+      });
+  }
+  
+  const random = Math.floor(Math.random() * 1000);
+  callService("https://api.quotable.io/random", displayQuote);
+  
